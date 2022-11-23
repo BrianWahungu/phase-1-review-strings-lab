2e89d13557235912fa38f42a8f5e7915525991a6
BrianWahungu <cepteinhawks@gmail.com>
2022-11-23 14:34:04 +0300

    completed new assignment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const currentUser = 'Grace Hopper';
+const currentUser = 'Sankara Jade';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+// const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
 
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
